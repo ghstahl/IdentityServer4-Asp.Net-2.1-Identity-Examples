aab68eb2da0101478a75282780378126f11e1b07
Herb Stahl <ghstahl@gmail.com>
2018-07-17 06:04:23 -0700

    initial

---
+++ src/AspNetCore.Identity.MultiFactor.Test.Neo4j/Models/TestFactor.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.Identity.MultiFactor.Test.Core;
+using AspNetCore.Identity.Neo4j;
+
+namespace AspNetCore.Identity.MultiFactor.Test.Neo4j.Models
+{
+    [Neo4jLabel("TestFactor")]
+    public class TestFactor : ChallengeFactor
+    {
+        public TestFactor(): base(Unique.S,Unique.S)
+        {
+
+        }
+        public TestFactor(string challenge, string challengeResponse) : base(challenge, challengeResponse)
+        {
+        }
+    }
+}
---
+++ src/AspNetCore.Identity.MultiFactor.Test.Neo4j/Models/TestUser.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCore.Identity.Neo4j;
+
+namespace AspNetCore.Identity.MultiFactor.Test.Neo4j.Models
+{
+    [Neo4jLabel("TestUser")]
+    public class TestUser : Neo4jIdentityUser
+    {
+    }
+}
