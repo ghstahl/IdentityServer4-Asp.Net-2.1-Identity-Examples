1e69e8c1811fa4ca84724fa6d02f1b9f6b68470b
Herb Stahl <ghstahl@gmail.com>
2018-07-17 18:52:59 -0700

    ui sugar

--- src/PagesWebApp.AspNetCoreNeo4j/Areas/Identity/Pages/Account/ManageFactors/Delete.cshtml.cs
+++ src/PagesWebApp.AspNetCoreNeo4j/Areas/Identity/Pages/Account/ManageFactors/Delete.cshtml.cs
@@ -1,16 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AspNetCore.Identity.Neo4j;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace PagesWebApp.Areas.Identity.Pages.Account.ManageFactors
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private IMultiFactorUserStore<ApplicationUser, ApplicationFactor> _multiFactorUserStore;
+        private UserManager<ApplicationUser> _userManager;
+        private ILogger<LoginModel> _logger;
+        [TempData] public string StatusMessage { get; set; }
+        [BindProperty]
+        public string FactorId { get; set; }
+
+        public ApplicationFactor Factor { get; set; }
+    
+        public DeleteModel(
+            UserManager<ApplicationUser> userManager,
+            IMultiFactorUserStore<ApplicationUser, ApplicationFactor> multiFactorUserStore,
+            ILogger<LoginModel> logger)
         {
+            _multiFactorUserStore = multiFactorUserStore;
+            _userManager = userManager;
+            _logger = logger;
+        }
+        public async Task OnGetAsync(string factorId)
+        {
+            FactorId = factorId;
+            Factor = await _multiFactorUserStore.FindByIdAsync(factorId,CancellationToken.None);
         }
     }
 }
