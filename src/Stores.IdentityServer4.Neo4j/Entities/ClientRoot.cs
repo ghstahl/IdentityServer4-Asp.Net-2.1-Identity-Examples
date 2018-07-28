5d9aa0cb8145f2a17e8e4a4277aa1a762e3b8b1d
ghstahl@gmail.com <ghstahl@gmail.com>
2018-07-28 11:18:34 -0700

    minor fixups

--- src/Stores.IdentityServer4.Neo4j/Entities/ClientRoot.cs
+++ src/Stores.IdentityServer4.Neo4j/Entities/ClientRoot.cs
@@ -12,7 +12,7 @@ namespace Stores.IdentityServer4.Neo4j.Entities
         public bool Enabled { get; set; } = true;
         public string ClientId { get; set; }
         public string ProtocolType { get; set; } = IdentityServerConstants.ProtocolTypes.OpenIdConnect;
-      
+    //  public List<ClientSecret> ClientSecrets { get; set; }
         public bool RequireClientSecret { get; set; } = true;
         public string ClientName { get; set; }
         public string Description { get; set; }
@@ -21,17 +21,18 @@ namespace Stores.IdentityServer4.Neo4j.Entities
         public bool RequireConsent { get; set; } = true;
         public bool AllowRememberConsent { get; set; } = true;
         public bool AlwaysIncludeUserClaimsInIdToken { get; set; }
-       
+ //     public List<ClientGrantType> AllowedGrantTypes { get; set; }
         public bool RequirePkce { get; set; }
         public bool AllowPlainTextPkce { get; set; }
         public bool AllowAccessTokensViaBrowser { get; set; }
-       
+   //   public List<ClientRedirectUri> RedirectUris { get; set; }
+   //   public List<ClientPostLogoutRedirectUri> PostLogoutRedirectUris { get; set; }
         public string FrontChannelLogoutUri { get; set; }
         public bool FrontChannelLogoutSessionRequired { get; set; } = true;
         public string BackChannelLogoutUri { get; set; }
         public bool BackChannelLogoutSessionRequired { get; set; } = true;
         public bool AllowOfflineAccess { get; set; }
-         
+   //   public List<ClientScope> AllowedScopes { get; set; }
         public int IdentityTokenLifetime { get; set; } = 300;
         public int AccessTokenLifetime { get; set; } = 3600;
         public int AuthorizationCodeLifetime { get; set; } = 300;
@@ -43,13 +44,14 @@ namespace Stores.IdentityServer4.Neo4j.Entities
         public int RefreshTokenExpiration { get; set; } = (int)TokenExpiration.Absolute;
         public int AccessTokenType { get; set; } = (int)0; // AccessTokenType.Jwt;
         public bool EnableLocalLogin { get; set; } = true;
-      
+  //    public List<ClientIdPRestriction> IdentityProviderRestrictions { get; set; }
         public bool IncludeJwtId { get; set; }
-        
+  //    public List<ClientClaim> Claims { get; set; }
         public bool AlwaysSendClientClaims { get; set; }
         public string ClientClaimsPrefix { get; set; } = "client_";
         public string PairWiseSubjectSalt { get; set; }
-
+  //    public List<ClientCorsOrigin> AllowedCorsOrigins { get; set; }
+ //     public List<ClientProperty> Properties { get; set; }
         public bool RequireRefreshClientSecret { get; set; }
         public bool AllowArbitraryLocalRedirectUris { get; set; }
     }
