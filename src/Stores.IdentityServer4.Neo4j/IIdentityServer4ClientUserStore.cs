c574544323b7187787e119357bfd968d4dc7c406
Herb Stahl <ghstahl@gmail.com>
2018-07-30 10:10:19 -0700

    refactoring

---
+++ src/Stores.IdentityServer4.Neo4j/IIdentityServer4ClientUserStore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Stores.IdentityServer4.Neo4j.Entities;
+
+namespace Stores.IdentityServer4.Neo4j
+{
+    public interface IIdentityServer4ClientUserStore<
+        TUser, 
+        TClient,
+        TSecret,
+        TGrantType,
+        TClaim,
+        TCorsOrigin,
+        TScope,
+        TIdPRestriction,
+        TProperty,
+        TPostLogoutRedirectUri,
+        TRedirectUri> :
+        IIdentityServer4ClientStore<
+            TClient,
+            TSecret,
+            TGrantType,
+            TClaim,
+            TCorsOrigin,
+            TScope,
+            TIdPRestriction,
+            TProperty,
+            TPostLogoutRedirectUri,
+            TRedirectUri>,
+        IDisposable
+        where TUser : class
+        where TClient : ClientRoot
+        where TSecret : Secret
+        where TGrantType : ClientGrantType
+        where TClaim : ClientClaim
+        where TCorsOrigin : ClientCorsOrigin
+        where TScope : ClientScope
+        where TIdPRestriction : ClientIdPRestriction
+        where TProperty : ClientProperty
+        where TPostLogoutRedirectUri : ClientPostLogoutRedirectUri
+        where TRedirectUri : ClientRedirectUri
+
+    {
+        Task CreateConstraintsAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IdentityResult> AddClientToUserAsync(TUser user, TClient client,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IList<TClient>> GetClientsAsync(TUser user,
+            CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
---
+++ src/Stores.IdentityServer4.Neo4j/IIdentityServer4GrantTypeStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Stores.IdentityServer4.Neo4j.Entities;
+
+namespace Stores.IdentityServer4.Neo4j
+{
+    public interface IIdentityServer4GrantTypeStore<TGrantType> :
+        IDisposable
+        where TGrantType : ClientGrantType
+    {
+        Task<IdentityResult> CreateGrantTypeAsync(TGrantType grantType,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IdentityResult> UpdateGrantTypeAsync(TGrantType originalGrantType, TGrantType grantType,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IdentityResult> DeleteGrantTypeAsync(TGrantType grantType,
+            CancellationToken cancellationToken = default(CancellationToken));
+        Task<IdentityResult> DeleteGrantTypesAsync(
+            CancellationToken cancellationToken = default(CancellationToken));
+        Task<TGrantType> FindGrantTypeAsync(string grantType,
+            CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
