d6ab023a696d105201c1643b47532671a9259ecc
ghstahl@gmail.com <ghstahl@gmail.com>
2018-08-01 08:23:54 -0700

    initial

---
+++ src/Stores.IdentityServer4.Neo4j/DTO/Neo4jIdentityServer4ClientRollup.cs
@@ -0,0 +1,13 @@
+﻿using Neo4jExtras;
+using StoresIdentityServer4.Neo4j.Entities;
+
+namespace StoresIdentityServer4.Neo4j
+{
+    /// <summary>
+    /// Represents an IdentityServer4 ClientRollup
+    /// </summary>
+    [Neo4jLabel(Neo4jConstants.Labels.ClientRollup)]
+    public class Neo4jIdentityServer4ClientRollup : ClientRollup
+    {
+    }
+}
