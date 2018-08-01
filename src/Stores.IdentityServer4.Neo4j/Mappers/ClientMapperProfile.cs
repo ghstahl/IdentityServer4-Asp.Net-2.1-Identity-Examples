d3c06728f7b0c8a025dc1289c451839c041766cd
ghstahl@gmail.com <ghstahl@gmail.com>
2018-08-01 08:18:11 -0700

    initial

---
+++ src/Stores.IdentityServer4.Neo4j/Mappers/ClientMapperProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using AutoMapper;
+using StoresIdentityServer4.Neo4j.Entities;
+
+namespace StoresIdentityServer4.Neo4j.Mappers
+{
+    /// <summary>
+    /// AutoMapper configuration for Client
+    /// Between model and entity
+    /// </summary>
+    public class ClientMapperProfile : Profile
+    {
+        /// <summary>
+        /// <see>
+        ///     <cref>{ClientMapperProfile}</cref>
+        /// </see>
+        /// </summary>
+        public ClientMapperProfile()
+        {
+            // entity to model
+            CreateMap<Client, IdentityServer4.Models.Client>(MemberList.Destination);
+            // model to entity
+            CreateMap<IdentityServer4.Models.Client, Entities.Client>(MemberList.Source);
+        }
+    }
+}
---
+++ src/Stores.IdentityServer4.Neo4j/Mappers/ClientMappers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+ 
+
+namespace StoresIdentityServer4.Neo4j.Mappers
+{
+    // <summary>
+    /// Extension methods to map to/from entity/model for clients.
+    /// </summary>
+    public static class ClientMappers
+    {
+        static ClientMappers()
+        {
+            Mapper = new MapperConfiguration(cfg => cfg.AddProfile<ClientMapperProfile>())
+                .CreateMapper();
+        }
+
+        internal static IMapper Mapper { get; }
+
+        /// <summary>
+        /// Maps an entity to a model.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        public static IdentityServer4.Models.Client ToModel(this Entities.Client entity)
+        {
+            return Mapper.Map<IdentityServer4.Models.Client>(entity);
+        }
+
+        /// <summary>
+        /// Maps a model to an entity.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns></returns>
+        public static Entities.Client ToEntity(this IdentityServer4.Models.Client model)
+        {
+            return Mapper.Map<Entities.Client>(model);
+        }
+    }
+}
