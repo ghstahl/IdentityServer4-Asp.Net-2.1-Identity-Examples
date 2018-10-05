6c458b48e4154386f77b4c2a4a62d3b6c13c51e4
ghstahl@gmail.com <ghstahl@gmail.com>
2018-10-05 07:36:53 -0700

    Baseline for a WebApp that will be the IDP for Support Agents. The agents have to login as themselves first so that later when this app is used as an IDP to proxy a user that support agent's information will be added to the final users id_token.

---
+++ src/PagesWebApp.AspNetCoreNeo4j.SupportAgent/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PagesWebApp.AspNetCoreNeo4j.SupportAgent.Data;
+
+[assembly: HostingStartup(typeof(PagesWebApp.AspNetCoreNeo4j.SupportAgent.Areas.Identity.IdentityHostingStartup))]
+namespace PagesWebApp.AspNetCoreNeo4j.SupportAgent.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ src/PagesWebApp.AspNetCoreNeo4j.SupportAgent/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PagesWebApp.AspNetCoreNeo4j.SupportAgent.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
